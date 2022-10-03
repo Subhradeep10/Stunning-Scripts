6895de42df7a54d9e8912ed2960e9529b466dcaf
aarjav24 <68058926+aarjav24@users.noreply.github.com>
2022-10-03 21:19:03 +0400

    Create variable for file name

--- file_converter.py
+++ file_converter.py
@@ -3,6 +3,7 @@ from PIL import Image
 filePath = input('Enter the path to the image: ')
 ext = filePath.split('/')[-1].split('.')[-1]
 folderPath = '/'.join(filePath.split('/')[:-1])
+fileName = filePath.split('/')[-1].split('.')[:-1]
 
 # im1 = Image.open('file name.jpg')  # path where the JPG is stored
 # im1.save('new file name.png')   # path where the PNG will be stored

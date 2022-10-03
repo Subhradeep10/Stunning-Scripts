3fbc77ee0d65902486748fd235e6c89bc241864a
aarjav24 <68058926+aarjav24@users.noreply.github.com>
2022-10-03 21:21:55 +0400

    Add conditional for final execution

--- file_converter.py
+++ file_converter.py
@@ -3,7 +3,14 @@ from PIL import Image
 filePath = input('Enter the path to the image: ')
 ext = filePath.split('/')[-1].split('.')[-1]
 folderPath = '/'.join(filePath.split('/')[:-1])
-fileName = filePath.split('/')[-1].split('.')[:-1]
+fileName = ''.join(filePath.split('/')[-1].split('.')[:-1])
+
+if ext == 'png':
+    img = Image.open(filePath)
+    img.save(f'{folderPath}/{fileName}.jpg')
+else:
+    img = Image.open(filePath)
+    img.save(f'{folderPath}/{fileName}.png')
 
 # im1 = Image.open('file name.jpg')  # path where the JPG is stored
 # im1.save('new file name.png')   # path where the PNG will be stored

e6906197d8b09e26e1d463021ab8d49d161563f0
aarjav24 <68058926+aarjav24@users.noreply.github.com>
2022-10-03 21:22:57 +0400

    Delete irrelevant comments

--- file_converter.py
+++ file_converter.py
@@ -11,6 +11,3 @@ if ext == 'png':
 else:
     img = Image.open(filePath)
     img.save(f'{folderPath}/{fileName}.png')
-
-# im1 = Image.open('file name.jpg')  # path where the JPG is stored
-# im1.save('new file name.png')   # path where the PNG will be stored
